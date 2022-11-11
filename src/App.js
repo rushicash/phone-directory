d7b88d0f9591f9c8154b3ee52f9373ca09586878
rushicash <115943653+rushicash@users.noreply.github.com>
2022-11-11 20:17:50 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,69 @@
 import Header from "./Header";
 import './App.css';
+import { Component } from "react";
+import { render } from "@testing-library/react";
 
-function App() {
-  // function deleteHandler() {
-  //   alert("Delete Clicked");
-  // }
+function clickHandler() {
+  alert("Delete Clicked");
+}
 
-  function clickHandler(e) {
-    
-    alert(e);
-  }
 
-  let subscribers = [
-    {
-      id: 1,
-      name: "Shilpa Bhat",
-      phone: "9999999999"
-    },
-    {
-      id: 2,
-      name: "Shristi",
-      phone: "8888888888"
-    },
-    { 
-      id: 3,
-      name:"Rushi",
-      phone: "8055438390"
+class App extends Component {
+
+  constructor() 
+  {
+    super();
+    this.state = {
+      subscribersListToShow: []
     }
-  ]
-  return (
-    <div>
-      <Header heading="Phone Directory"/>
-      <div className="component-body-container">
-        <button className="custom-btn add-btn">Add</button>
-        <div className="grid-container header-container">
-            <span className="grid-item name-heading">Name</span>
-            <span className="grid-item phone-heading">Phone</span>
-        </div>
-        
-        {
-          subscribers.map(sub => {
-            return <div key={sub.id} className="grid-container">
-              <span className="grid-item">{sub.name}</span>
-              <span className="grid-item">{sub.phone}</span>
-              <span className="grid-item action-btn-container"> 
-                <button className="custom-btn delete-btn" onClick={clickHandler.bind(this, "Delete this message")}>Delete</button> 
-              </span>
-            </div>
-          })
-        }
+  }
+  
+  
 
-      </div>
-    </div>  
-  );
+  // let subscribers = [
+  //   {
+  //     id: 1,
+  //     name: "Shilpa Bhat",
+  //     phone: "9999999999"
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Shristi",
+  //     phone: "8888888888"
+  //   },
+  //   { 
+  //     id: 3,
+  //     name:"Rushi",
+  //     phone: "8055438390"
+  //   }
+  // ]
+  render(){
+    return (
+      <div>
+        <Header heading="Phone Directory"/>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container header-container">
+              <span className="grid-item name-heading">Name</span>
+              <span className="grid-item phone-heading">Phone</span>
+          </div>
+          
+          {
+            this.state.subscribersListToShow.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container"> 
+                  <button className="custom-btn delete-btn" onClick={clickHandler.bind(this, "Delete this message")}>Delete</button> 
+                </span>
+              </div>
+            })
+          }
+
+        </div>
+      </div>  
+    );
+  }
 }
 
 export default App;
